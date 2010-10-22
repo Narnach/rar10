75e3f6ed3c0651206b02b27bf7cb352aac966cc3
smeevil <info@govannon.nl>
2010-10-22 14:09:07 +0200

    navigation fixed, added first top 5 results of genres

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,12 @@
 class Tag < ActiveRecord::Base
   has_friendly_id :name, :use_slug=>true
   has_and_belongs_to_many :artists
+  
+  def self.most_populair_genres(amount=10)
+    tags=Hash.new
+    Tag.all.each do |t|
+      tags[t.name]=t.artists.count
+    end
+    tags=tags.sort_by(&:last).reverse[0..amount]
+  end
 end
